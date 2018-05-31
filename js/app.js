9f273a3bf0607d5f192577ec4fe584f152249259
Husamoa <pawebial@gmail.com>
2018-05-31 20:49:18 +0200

    calculator completed

--- js/app.js
+++ js/app.js
@@ -55,7 +55,7 @@ document.addEventListener("DOMContentLoaded", function () {
         nextPicture.addEventListener('click', function () {
             console.log('nextPicture');
             listImg[count].classList.remove('visible');
-            if(count >= listImg.length - 1) {
+            if (count >= listImg.length - 1) {
                 count = 0;
             } else {
                 count++;
@@ -96,16 +96,75 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function choose() {
         var listPanel = document.querySelectorAll('.list_panel li');
-        var value = listPanel.value;
+        var listLabel = document.getElementsByClassName('list_label');
+        var transportCheck = document.getElementById('transport');
+        var value = document.querySelectorAll('.panel_right .value');
+
+        var title = document.querySelector('.title');
+        var color = document.querySelector('.color');
+        var pattern = document.querySelector('.pattern');
+        var transport = document.querySelector('.transport');
+
+        var sumText = document.querySelector('.sum strong');
+
+        var sum = 0;
+
+
         for (var i = 0; i < listPanel.length; i++) {
-
-
             listPanel[i].addEventListener('click', function () {
                 this.parentElement.previousElementSibling.previousElementSibling.innerText = this.innerText;
                 this.parentElement.previousElementSibling.previousElementSibling.style.color = 'black';
-                this.parentElement.classList.toggle('show');
+                title.innerText = "Chair " + listLabel[0].innerText;
 
-            })
+                if (listLabel[0].innerText === 'Clair') {
+                    value[0].innerText = 200;
+                    sum = 200;
+                    sumText.innerText = sum;
+                } else if (listLabel[0].innerText === 'Margarita') {
+                    value[0].innerText = 300;
+                    sum = 300
+                    sumText.innerText = sum;
+                } else if (listLabel[0].innerText === 'Selena') {
+                    value[0].innerText = 400;
+                    sum = 400;
+                    sumText.innerText = sum;
+                } else {
+                    value[0].innerText = '';
+                }
+
+                color.innerText = listLabel[1].innerText;
+                if (listLabel[1].innerText === 'Czerwony') {
+                    value[1].innerText = 40;
+                    sum += 40;
+                    sumText.innerText = sum;
+                } else if (listLabel[1].innerText === 'Czarny') {
+                    value[1].innerText = 50;
+                    sum += 50;
+                    sumText.innerText = sum;
+                } else if (listLabel[1].innerText === 'Pomarańczowy') {
+                    value[1].innerText = 60;
+                    sum += 60;
+                    sumText.innerText = sum;
+                } else {
+                    value[1].innerText = '';
+                }
+
+                pattern.innerText = listLabel[2].innerText;
+                if (listLabel[2].innerText === 'Tkanina') {
+                    value[2].innerText = 100;
+                    sum += 100;
+                    sumText.innerText = sum;
+                } else if (listLabel[2].innerText === 'Skóra') {
+                    value[2].innerText = 200;
+                    sum += 200;
+                    sumText.innerText = sum;
+                } else {
+                    value[2].innerText = '';
+                }
+
+                
+                this.parentElement.classList.toggle('show');
+            });
 
             listPanel[i].addEventListener('mousemove', function f() {
                 this.style.backgroundColor = 'lightblue';
@@ -113,9 +172,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
             listPanel[i].addEventListener('mouseleave', function f() {
                 this.style.backgroundColor = '';
-
             });
         }
+
+        transportCheck.addEventListener("change", function () {
+            if (this.checked) {
+                transport.innerText = 'Transport';
+                value[3].innerText = 200;
+                sum += 200;
+                sumText.innerText = sum;
+            } else {
+                transport.innerText = '';
+                value[3].innerText = '';
+                sum -= 200;
+                sumText.innerText = sum;
+            }
+        });
+
+
     }
 
     choose();
